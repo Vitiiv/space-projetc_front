7b4f428f1221845752d4967c0d64a283b8baa680
Vitiiv <victhug2004@gmail.com>
2025-11-14 15:34:55 -0300

    Projeto concluído

---
+++ gallery/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'gallery/index.html')
+
+
+def imagem(request):
+    return render(request, 'gallery/imagem.html')
